26bbdf95c84dcd84e5cc5a4b446bd65afda94fba
Nishanth Shankar <nishanth@Nishanths-MacBook-Pro.local>
2023-03-11 15:01:14 +0530

    Add tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,8 @@
   import { base } from "$app/paths";
 </script>
 
-<h1>sveltekit-gh-pages</h1>
+<h1 class="bg-red-100">sveltekit-gh-pages</h1>
+sd
 <p>Deployeds to GitHub Pages</p>
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,14 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from '@sveltejs/adapter-static'
+import { vitePreprocess } from '@sveltejs/kit/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter(),
     paths: {
-      base: process.env.NODE_ENV === "production" ? "/sveltekit-gh-pages" : "",
-    },
-  },
-};
+      base: process.env.NODE_ENV === 'production' ? '/sveltekit-gh-pages' : ''
+    }
+  }
+}
 
-export default config;
+export default config
